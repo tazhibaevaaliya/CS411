fbe7a2e80f05c4ae94673a304290a07e8a6b5584
jesseConde <jconde@bu.edu>
2022-12-03 14:01:20 -0500

    commit message

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,35 +1,13 @@
 import "./App.css";
-import axios from "axios";
-import React, { useState } from "react";
+import { Search } from "./Search";
+import { useState } from "react";
 
 function App() {
-  const [artist, setArtist] = useState("");
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    axios.post("http://localhost:4000", { artist: artist }).then((res) => {
-      console.log(res);
-    });
-  }
-
-  const handleChange = (event) => {
-    setArtist(event.target.value);
-  };
-
+  const [isToggled, setIsToggled] = useState(false);
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Artist Name:
-          <input
-            type="text"
-            value={artist}
-            name="zip"
-            onChange={handleChange}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+    <div className="App">
+      <button onClick={() => setIsToggled(!isToggled)}>Toggle</button>
+      {isToggled && <Search />}
     </div>
   );
 }
